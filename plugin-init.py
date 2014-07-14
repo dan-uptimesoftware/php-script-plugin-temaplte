fb67d2b2f0c419fbd0b74ede244d8bb8fbb8a964
dan branton <daniel.branton@uptimesoftware.com>
2014-07-13 21:44:31 -0400

    plugin-init now updates xml contents

--- plugin-init.py
+++ plugin-init.py
@@ -9,3 +9,16 @@ new_name = sys.argv[1]
 os.rename("pkg/files/plugins/scripts/" + current_name, "pkg/files/plugins/scripts/" + new_name)
 os.rename("pkg/files-posix/plugins/scripts/" + current_name, "pkg/files-posix/plugins/scripts/" + new_name)
 os.rename("pkg/files-win/plugins/scripts/" + current_name, "pkg/files-win/plugins/scripts/" + new_name)
+
+#rename the xml file in pkg & update contents
+old_xml = open("pkg/" + current_name + ".xml", 'r')
+new_xml = open("pkg/" + new_name + ".xml", 'w')
+
+for line in old_xml:
+    new_xml.write(line.replace(current_name, new_name))
+
+old_xml.close()
+new_xml.close()
+
+
+
