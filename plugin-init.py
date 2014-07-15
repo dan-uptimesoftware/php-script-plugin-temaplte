2fce37c4f8e97b5c7f2021f7af78ea79a61ecf85
dan <daniel.branton@uptimesoftware.com>
2014-07-15 13:34:29 -0400

    added some actual output to plugin_init.py script

--- plugin-init.py
+++ plugin-init.py
@@ -5,20 +5,48 @@ import os, sys
 current_name = "php-script-plugin-template"
 new_name = sys.argv[1]
 
+
+def update_folder(path, current_name, new_name):
+	os.rename(path + current_name, path + new_name)
+	if( os.path.exists(path + new_name)):
+		print ("OK: " + path + new_name + " updated")
+	else:
+		print ("Error: " +  + path + new_name + "failed to update")
+
+
+print ("updating plugin with new name: " + new_name + "\n")
+
 #update the directory names in the various files subfolders
-os.rename("pkg/files/plugins/scripts/" + current_name, "pkg/files/plugins/scripts/" + new_name)
-os.rename("pkg/files-posix/plugins/scripts/" + current_name, "pkg/files-posix/plugins/scripts/" + new_name)
-os.rename("pkg/files-win/plugins/scripts/" + current_name, "pkg/files-win/plugins/scripts/" + new_name)
+print ("Updating Folder Paths:")
+update_folder("pkg/files/plugins/scripts/", current_name, new_name)
+update_folder("pkg/files-posix/plugins/scripts/", current_name, new_name)
+update_folder("pkg/files-win/plugins/scripts/", current_name, new_name)
+
 
 #rename the xml file in pkg & update contents
-old_xml = open("pkg/" + current_name + ".xml", 'r')
-new_xml = open("pkg/" + new_name + ".xml", 'w')
+print "Creating and updating " + new_name + ".xml with new name & paths"
+old_xml_path = "pkg/" + current_name + ".xml"
+new_xml_path = "pkg/" + new_name + ".xml"
+old_xml = open(old_xml_path, 'r')
+new_xml = open(new_xml_path, 'w')
 
 for line in old_xml:
     new_xml.write(line.replace(current_name, new_name))
 
 old_xml.close()
 new_xml.close()
+
+if (os.path.exists(new_xml_path)):
+	print (new_xml_path + " exists")
+	if (os.remove(old_xml_path)):
+		print ("Removed old xml file: " + old_xml_path)
+	else:
+		print ("Unable to remove old xml: " + old_xml_path)
+		print ("Try deleting file manually")
+else:
+	print (new_xml_path +"does not exist?")
+
+
 
 
 
